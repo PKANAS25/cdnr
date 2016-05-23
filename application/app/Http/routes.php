5992c9330001507ad47a6f77c4d22ac9233cd37c
vagrant <vagrant@homestead>
2016-05-23 13:11:19 +0000

    Initial commit

---
+++ application/app/Http/routes.php
@@ -0,0 +1,130 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+Route::get('/', 'PagesController@index');
+Route::get('/home', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/products', 'PagesController@products');
+Route::get('/careers', 'PagesController@careers');
+Route::get('/contact', 'PagesController@contact');
+Route::post('/contact', 'PagesController@contactSend');
+
+
+Route::get('/hrm', 'Auth\AuthController@getLogin');
+Route::post('/hrm', 'Auth\AuthController@authenticate');
+Route::get('/hrm/logout', 'Auth\AuthController@getLogout');
+Route::get('/hrm/errorLogout', 'Auth\AuthController@errorLogout');
+
+// Password reset link request routes...
+Route::get('/hrm/password/email', 'Auth\PasswordController@getEmail');
+Route::post('/hrm/password/email', 'Auth\PasswordController@postEmail');
+
+// Password reset routes...
+Route::get('/hrm/password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('/hrm/password/reset', 'Auth\PasswordController@postReset');
+
+Route::group(array('middleware' => 'auth' ), function () {
+
+Route::get('hrm/home', 'HomeController@home');
+
+//---------------------------------UsersController--------------------------------------------------------------------
+    Route::get('hrm/users',['uses'=>'UsersController@index']);
+	Route::get('hrm/users/{id?}/edit', ['middleware' => 'Superman','uses'=>'UsersController@edit']);    
+	Route::post('hrm/users/{id?}/edit',['middleware' => 'Superman','uses'=>'UsersController@update']);
+
+    Route::get('hrm/users/register',['middleware' => 'UserAdd', function () {return view('users.register');}]);
+    Route::get('/duplicateCheck', ['uses'=>'UsersController@duplicateCheck']); 
+    Route::post('hrm/users/register',['middleware' => 'UserAdd','uses'=>'UsersController@add']); 
+     
+    Route::get('/users/{id?}/disable', ['middleware' => 'UserAdd','uses'=>'UsersController@disable']);
+
+    Route::get('hrm/users/disabled',['middleware' => 'UserAdd','uses'=>'UsersController@usersDisabled']);
+    Route::get('/users/{id?}/restore', ['middleware' => 'UserAdd','uses'=>'UsersController@restore']);
+
+    Route::get('/users/{id?}/password', ['middleware' => 'UserAdd','uses'=>'UsersController@passwordChangeView']);
+    Route::post('/users/{id?}/password', ['middleware' => 'UserAdd','uses'=>'UsersController@passwordChange']);
+
+
+    Route::get('users/password/edit/self',function () {return view('users.passwordSelfEdit');});
+    Route::post('users/password/edit/self', 'UsersController@passwordSelfUpdate');
+
+//-------------------------------RolesController-----------------------------------------------
+	Route::get('hrm/roles', ['middleware' => 'UserAdd','uses'=>'RolesController@index']);
+	Route::get('hrm/roles/create', ['middleware' => 'Superman','uses'=>'RolesController@create']);
+	Route::post('hrm/roles/create', ['middleware' => 'Superman','uses'=>'RolesController@store']);
+
+//-------------------------------ClientsController-----------------------------------------------    
+ 
+    Route::get('hrm/clients/add', ['middleware' => 'ClientAdd','uses'=>'ClientsController@add']);
+    Route::get('/cityLoader', 'ClientsController@cityLoader'); 
+    Route::get('/clientAddCheck', 'ClientsController@clientAddCheck');
+    Route::post('hrm/clients/add', ['middleware' => 'ClientAdd','uses'=>'ClientsController@save']); 
+
+    Route::get('hrm/clients/{id?}/profile', 'ClientsController@profile');
+
+    Route::get('hrm/clients/index', 'ClientsController@index');
+
+    Route::get('hrm/clients/{id?}/edit', ['middleware' => 'ClientAdd','uses'=>'ClientsController@edit']);
+    Route::post('hrm/clients/{id?}/edit', ['middleware' => 'ClientAdd','uses'=>'ClientsController@editProcess']);
+    Route::get('/clientEditCheck', 'ClientsController@clientEditCheck');
+
+//-------------------------------ContactsController-----------------------------------------------    
+
+    Route::get('hrm/contacts/{id?}/add', ['middleware' => 'ContactAdd','uses'=>'ContactsController@add']);
+    Route::get('/contactAddCheck', 'ContactsController@contactAddCheck');
+    Route::post('hrm/contacts/{id?}/add', ['middleware' => 'ContactAdd','uses'=>'ContactsController@save']);
+
+    Route::post('/deleteClientContact', ['middleware' => 'ContactAdd','uses'=>'ContactsController@deleteClientContact']);
+    Route::post('/restoreClientContact', ['middleware' => 'ContactAdd','uses'=>'ContactsController@restoreClientContact']);
+
+    Route::get('hrm/contacts/{clientId?}/{contactId?}/edit', ['middleware' => 'ContactAdd','uses'=>'ContactsController@edit']);
+    Route::get('/contactEditCheck', 'ContactsController@contactEditCheck');
+    Route::post('hrm/contacts/{clientId?}/{contactId?}/edit', ['middleware' => 'ContactAdd','uses'=>'ContactsController@editProcess']);
+
+    Route::get('hrm/contacts/search', 'ContactsController@initiate');  
+    Route::post('hrm/contacts/search', 'ContactsController@filterShow');  
+
+    Route::get('hrm/contactSearchBind', 'ContactsController@searchBind');
+
+//-------------------------------ClientsControllerExtra-----------------------------------------------    
+ 
+    Route::get('hrm/clients/filter', 'ClientsControllerExtra@initiate');  
+    Route::post('hrm/clients/filter', 'ClientsControllerExtra@filterShow');   
+
+//-------------------------------CallsController-----------------------------------------------    
+
+    Route::get('hrm/calls/{id?}/add',  'CallsController@add');
+    Route::post('hrm/calls/{id?}/add',  'CallsController@save');
+
+    Route::get('hrm/calls/{clientId?}/{contactId?}/edit','CallsController@edit');
+    Route::post('hrm/calls/{clientId?}/{contactId?}/edit','CallsController@editProcess');
+
+    Route::post('/hrm/calls/{clientId?}/{id?}/delete','CallsController@delete');
+
+//-------------------------------SettingsController-----------------------------------------------    
+
+    Route::get('hrm/settings/industries',  'SettingsController@industries');
+    Route::get('/industryAddCheck', 'SettingsController@industryAddCheck');
+    Route::post('hrm/settings/industries',  'SettingsController@saveIndustry');
+
+
+    Route::get('hrm/settings/designations',  'SettingsController@positions');
+    Route::get('/positionAddCheck', 'SettingsController@positionAddCheck');
+    Route::post('hrm/settings/designations',  'SettingsController@savePosition');
+
+//-------------------------------ExcelController-----------------------------------------------    
+
+    Route::get('/excelClientsList',  'ExcelController@clientsList');
+    Route::get('/excelContactsFilter/{position}/{industry}/{country}/{city}',  'ExcelController@contactsFilter');
+
+     Route::get('/hrm/test',  'testController@index');
+});
---
+++ application/resources/views/shared/hrNavbar.blade.php
@@ -0,0 +1,84 @@
+<link href="/hr_assets/plugins/ionicons/css/ionicons.min.css" rel="stylesheet" />
+<div id="sidebar" class="sidebar">
+            <!-- begin sidebar scrollbar -->
+            <div data-scrollbar="true" data-height="100%">
+                <!-- begin sidebar user -->
+                <ul class="nav">
+                    <li class="nav-profile">
+                         
+                        <div class="info">
+                           
+                             {!! Auth::user()->name; !!} 
+                            <small></small>
+                        </div>
+                    </li>
+                </ul>
+                <!-- end sidebar user -->
+                <!-- begin sidebar nav -->
+                <ul class="nav">
+                    <li class="nav-header">Navigation</li>
+<!-- **************************************************************************************************************************************** -->                    
+                    <li @if(session('title') == 'Home') class="active" @endif><a href="/hrm/home"><i class="fa fa-home"></i> <span>Dashboard</span></a></li>
+<!-- ***********************************************************Payments***************************************************************************** -->                   
+                      
+                    <li class="has-sub @if(session('title') == 'Clients')   active @endif">
+                        <a href="javascript:;">
+                             <b class="caret pull-right"></b>
+                            <i class="fa fa-institution alias"></i> 
+                            <span>Clients</span>
+                        </a>
+                        <ul class="sub-menu">
+                            <li @if(session('subtitle') == 'clientsList') class="active" @endif><a href="\hrm\clients\index">List of Clients</a></li>
+                            @if(Auth::user()->hasRole('ClientAdd'))
+                            <li @if(session('subtitle') == 'addClient') class="active" @endif><a href="\hrm\clients\add">Add Client</a></li>
+                            @endif
+                            <li @if(session('subtitle') == 'filterClient') class="active" @endif><a href="\hrm\clients\filter">Filter <i class="fa fa-filter text-theme m-l-5"></i></a></li> 
+                            <li @if(session('subtitle') == 'filterContact') class="active" @endif><a href="\hrm\contacts\search">Search Contacts <i class="fa fa-binoculars text-theme m-l-5"></i></a></li>  
+                        </ul>
+                    </li>
+ <!-- **********************************************************Administrator****************************************************************************** -->                    
+                    @if( Auth::user()->hasRole('user_add') || Auth::user()->hasRole('Superman'))
+                    <li class="has-sub @if(session('title') == 'Administrator')   active @endif" >
+                        <a href="javascript:;">
+                            <b class="caret pull-right"></b>
+                            <i class="ion-star"></i>
+                            <span>Administrator</span>
+                        </a>
+                        <ul class="sub-menu">
+                            @if(Auth::user()->hasRole('user_add'))
+                            <li @if(session('subtitle') == 'users') class="active" @endif><a href="/hrm/users">Users</a></li> 
+                            <li @if(session('subtitle') == 'register') class="active" @endif><a href="/hrm/users/register">Add Users</a></li>@endif
+                            @if(Auth::user()->hasRole('Superman'))
+                            <li @if(session('subtitle') == 'Roles') class="active" @endif><a href="/hrm/roles">Roles</a></li>
+                            <li @if(session('subtitle') == 'addRoles') class="active" @endif><a href="/hrm/roles/create">Add Roles</a></li>@endif
+                             
+                        </ul>
+                    </li>
+                    @endif
+ <!-- *************************************************************Settings*************************************************************************** -->                    
+                    @if( Auth::user()->hasRole('ContactAdd') || Auth::user()->hasRole('ClientAdd'))
+                    <li class="has-sub @if(session('title') == 'Settings')   active @endif" >
+                        <a href="javascript:;">
+                            <b class="caret pull-right"></b>
+                            <i class="fa fa-cogs"></i>
+                            <span>Settings</span>
+                        </a>
+                        <ul class="sub-menu">
+                            @if( Auth::user()->hasRole('ClientAdd'))<li @if(session('subtitle') == 'industries') class="active" @endif><a href="/hrm/settings/industries">Industries</a></li>@endif
+                            @if( Auth::user()->hasRole('ContactAdd'))<li @if(session('subtitle') == 'designations') class="active" @endif><a href="/hrm/settings/designations">Positions</a></li>@endif
+                        </ul>
+                    </li>
+                    @endif
+                     
+                     
+                     
+                     
+                    <!-- begin sidebar minify button -->
+                    <li><a href="javascript:;" class="sidebar-minify-btn" data-click="sidebar-minify"><i class="fa fa-angle-double-left"></i></a></li>
+                    <!-- end sidebar minify button -->
+                </ul>
+                <!-- end sidebar nav -->
+            </div>
+            <!-- end sidebar scrollbar -->
+        </div>
+        <div class="sidebar-bg"></div>
